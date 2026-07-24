23f17cb72626acebc3b637893be5f8ba1d58f7a0
Samuelmdlow <samuel.m.d.low@gmail.com>
2026-07-24 05:25:41 +0000

    Add VectorExtension to migration file

--- attachments/migrations/0005_alter_attachment_options_attachmentcontent.py
+++ attachments/migrations/0005_alter_attachment_options_attachmentcontent.py
@@ -3,7 +3,7 @@
 import django.db.models.deletion
 import pgvector.django.vector
 from django.db import migrations, models
-
+from pgvector.django import VectorExtension
 
 class Migration(migrations.Migration):
 
@@ -12,6 +12,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        VectorExtension(),
         migrations.AlterModelOptions(
             name="attachment",
             options={"ordering": ["-published_at"]},
