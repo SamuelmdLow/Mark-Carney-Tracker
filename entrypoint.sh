4525e6b626c0c0e6d59e85e84a5399f0688eb905
Samuelmdlow <samuel.m.d.low@gmail.com>
2026-08-05 03:40:11 +0000

    Add caching to github workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,3 @@
-pip install -r requirements.txt
-python manage.py makemigrations &&
 python manage.py migrate &&
 python manage.py collectstatic --noinput &&
 gunicorn pm_tracker.wsgi:application --bind 0.0.0.0:8000
