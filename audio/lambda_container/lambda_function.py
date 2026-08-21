636e020c64e407c025b8514858063680d4ec2cc5
Samuelmdlow <samuel.m.d.low@gmail.com>
2026-08-21 20:58:50 +0000

    Reduce responsibility of lambda transcription function

--- audio/lambda_container/lambda_function.py
+++ audio/lambda_container/lambda_function.py
@@ -1,18 +1,14 @@
 import json
 import whisper
-from shared.services import M3U8
+from shared.services import transcribe_audio
 
 model = whisper.load_model('turbo', download_root="tmp/whisper")
 
 def handler(event, context):
-    m3u8_base_url = event['video_m3u8']
+    audio = event['audio']
 
     initial_prompt = None
     if "initial_prompt" in event:
         initial_prompt = event['initial_prompt']
 
-    m3u8 = M3U8()
-    m3u8.load(m3u8_base_url)
-    segments = m3u8.transcribe(model, initial_prompt=initial_prompt)
-
-    return segments
+    return transcribe_audio(model, audio, initial_prompt)
