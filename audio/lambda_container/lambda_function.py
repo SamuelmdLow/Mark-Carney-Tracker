72b0f5a6ad61b92a2d63f9ea318596df38d798fa
Samuelmdlow <samuel.m.d.low@gmail.com>
2026-08-21 12:28:33 +0000

    Use correct param in lambda hander (oops)

--- audio/lambda_container/lambda_function.py
+++ audio/lambda_container/lambda_function.py
@@ -5,11 +5,11 @@ from shared.services import M3U8
 model = whisper.load_model('turbo', download_root="tmp/whisper")
 
 def handler(event, context):
-    m3u8_base_url = context['video_m3u8']
+    m3u8_base_url = event['video_m3u8']
 
     inital_prompt = None
-    if "initial_promt" in context:
-        initial_prompt = context['inital_prompt']
+    if "initial_promt" in event:
+        initial_prompt = event['inital_prompt']
 
     m3u8 = M3U8()
     m3u8.load(m3u8_base_url)
